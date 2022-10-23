fab61e72f120436604b97c5a3e88fcb37b4697a3
Clipper4rancione <meluziis.ciro99@gmail.com>
2022-10-23 18:54:27 +0200

    added functions for endGame and bombs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
+
+//file creato seguendo la logica della lezione e appunti presi dalla stessa, ragionando ed andando avanti solo dopo aver capito il passaggio precedente.
+
+
+
 const main = document.querySelector('.game-wrapper');
 const playBtn = document.querySelector('#play');
 const levelSelect = document.querySelector('#level');
 
 const gridLevels = [100, 81, 49];
-let bombsNumber = 16;
+let bombsNumber = 3;
 let bombs = [];
 
 let score = 0;
@@ -13,12 +18,11 @@ playBtn.addEventListener('click', play);
 function play(){
     //STABILISCO IL NUMERO DELLE CELLE
     const boxNumbers = gridLevels[levelSelect.value];
-    console.log(boxNumbers);
 
     reset();
 
     generateGround(boxNumbers);
-    bombs.push(bombGenerator(boxNumbers));
+    bombs = bombGenerator(boxNumbers);
     console.log(bombs);
 }
 
@@ -48,12 +52,60 @@ function generateBox(boxId, boxNumbers){
 
 function handleClick(){
     if(!bombs.includes(this.boxId)){
-        score++
+
+        this.classList.add('clicked');
+        score++;
         console.log(score);
+
+        //CREO LA COLLECTION CHE CONTIENE TUTTE LE CELLE
+        const boxes = document.getElementsByClassName('box');
+        //CREO LA CONDIZIONE DI VITTORIA DEFINITIVA
+        if(score === boxes.length - bombsNumber){
+            console.log('HAI VINTO');
+            endGame(true)
+        }
     }else{
-        console.log('FINE');
+        console.log('hai perso');
+        endGame(false)
     }
 }
+
+function endGame(isWin){
+
+    /*
+    Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente
+     ha cliccato su una cella che non era una bomba.
+    */
+    let msg;
+    const boxes = document.getElementsByClassName('box');
+
+    if(isWin){
+        msg = ` HAI Vinto! <br>  
+                Hai fatto ${score} punti su ${boxes.length - bombsNumber}`;
+        console.log('HAI VINTO');
+    }else{
+        msg = ` HAI PERSO <br> 
+                Hai calpestato una bomba :( <br> 
+                Hai fatto ${score} punti su ${boxes.length - bombsNumber}`;
+        console.log('HAI PERSO');
+        //mi accende tutte le bombe 
+        showBombs();
+    }
+
+    document.querySelector('.end-message').innerHTML = msg;
+}
+
+//creo la funzione che mi accende tutte le bombe
+function showBombs(){
+
+    const boxes = document.getElementsByClassName('box');
+    for(let i = 0; i < boxes.length; i++){
+        const box = boxes[i];
+        if(bombs.includes(box.boxId)){
+            box.classList.add('bomb');
+        }
+    }
+};
 
 function bombGenerator(boxNumbers){
     const bombsGenerated = [];
@@ -74,4 +126,6 @@ function randomNumberGenerator(min, max){
 function reset(){
     main.innerHTML = '';
     score = 0;
+    document.querySelector('.end-message').innerHTML = '';
+
 }
